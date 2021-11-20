b85b0c0be149804e69ae9ca1f49d2f7751e95c25
jacobnathanson <jacobnath93@gmail.com>
2021-11-20 12:19:02 -0500

    used jquery and sass on login and reserve modals

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 $(function () {
-    $(".carousel").carousel({
-      interval: 2000
-    });
-    $('#carouselButton').click(function () {
-      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-        $(".carousel").carousel("pause");
-        $("#carouselButton").children("i").removeClass("fa-pause");
-        $("#carouselButton").children("i").addClass("fa-play");
-      }else{
-        $(".carousel").carousel("cycle");
-        $("#carouselButton").children("i").removeClass("fa-play");
-        $("#carouselButton").children("i").addClass("fa-pause");
-      }
-    });
-  })
+  // Carousel
+  $(".carousel").carousel({
+    interval: 2000
+  });
+  $('#carouselButton').click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
+
+
+
+  // Reserve Campsite
+  $("#reserveButton").click(function () {
+    $("#reserveModal").modal('show');
+  });
+
+  // Login
+  $("#loginButton").click(function () {
+    $("#loginModal").modal('show');
+  });
+
+
+
+})
